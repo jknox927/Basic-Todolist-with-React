f35a08688f11600045aa64f97117bf01bcbd5fa8
jknox927 <joshua.knox927@gmail.com>
2023-01-31 02:15:16 +0000

    Init commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	// Handle input on Enter key press
+	const pressEnter = (e) => {
+		if (e.key === "Enter") {
+			setTodos(todos.concat([inputValue]));
+			setInputValue("");
+		}
+	}
+
+	// Handle delete on icon click
+	const clickDelete = (index) => {
+		setTodos(
+			todos.filter(
+				(t, currentIndex) =>
+				index != currentIndex
+			)
+		)
+	}
+
+  return (
+    <div className="container">
+      <h1 className="text-center">todos</h1>
+      <ul>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          onKeyDown={pressEnter}
+          placeholder="What needs to be done?"
+        />
+		{/*
+			For each item in 'todos' and the index,
+			Create a <li> element.
+			On click function filters each item in the array
+			to delete the clicked item.
+		*/}
+        {todos.map((item, index) => (
+          <li>
+            {item}{" "}
+            <i
+              className="fas fa-trash-alt"
+              onClick={() => clickDelete(index)}
+            ></i>
+          </li>
+        ))}
+		<div className="tasksLeft">{todos.length} items left</div>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
